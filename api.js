7d7176aebc264793b9ba0dfd7dc4f24489a0852d
Philip Hayes <phayes@redhat.com>
2019-09-19 15:52:05 -0400

    Added parameter checking

--- api.js
+++ api.js
@@ -11,7 +11,9 @@ function apiRoute() {
   route.use(cors());
   route.use(bodyParser());
   route.post('/user', function(req, res, next) {
-    
+    if (!req.body || !req.body.dbName || !req.body.username || !req.body.password ) {
+        return res.json({err: "Required parameter missing: dbName, username, password"})
+    }
     dbTest.createUser(req.body.dbName, req.body.username, req.body.password, function(err, response){
         if (err) {
             res.json({err: err})
