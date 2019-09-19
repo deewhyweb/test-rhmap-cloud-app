6a9d46de69ef03e933b880cd49c6349ee7e7525f
Philip Hayes <phayes@redhat.com>
2019-09-19 12:08:50 -0400

    added keycloak

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-"use strict";
-
-var express = require("express"),
-  app = (module.exports = express()),
-  bodyParser = require("body-parser");
-const Keycloak = require("keycloak-connect");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+const probe = require('kube-probe');
+probe(app);
+const Keycloak = require('keycloak-connect');
 
 var keycloak = new Keycloak({});
-
 app.use(keycloak.middleware());
 
-app.use(require("cors")());
+// Enable CORS for all requests
+app.use(cors());
 
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(
-  bodyParser.urlencoded({
-    limit: "10mb",
-    extended: true,
-    parameterLimit: 50000
-  })
-);
+app.use('/', keycloak.protect(), require('./api.js')());
 
-app.use("/", keycloak.protect(), require("./api.js")());
+// allow serving of static files from the public directory
+app.use(express.static(__dirname + '/public'));
+
+function logErrors(err, req, res, next) {
+  console.error(err);
+}
+
+app.use(logErrors);
 
 var port = process.env.PORT || 8004;
-app.listen(port, function() {
-  console.log("App started at: %s on port %d", new Date(), port);
+var host = '0.0.0.0';
+app.listen(port, host, function() {
+  console.log('App started at: ' + new Date() + ' on port: ' + port);
 });
